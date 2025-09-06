d4d796172aaf4ff968d282c9711fcfa132e8f618
Imran Basha M <47586246+immy1989@users.noreply.github.com>
2025-09-06 08:50:38 +0530

    Add files via upload

---
+++ Week1/Classroom Assignments/ClassroomAssingnment2.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age:"))
+exp = int(input("Enter your total years of experience:"))
+
+if(age >= 22 and exp >= 2):
+ print("Access Granted")
+else:
+ print("Access Denied")
